4c281b28cee4496e013ea8940cb57352994ebcf6
aderiko <aderikoprasetya99@gmail.com>
2022-12-22 20:39:29 +0700

    crud

---
+++ index1.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello World!<br>";
+
+$nama = "Aderiko";
+$umur = 22;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Arafi";
+$umurAdik = 15;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
